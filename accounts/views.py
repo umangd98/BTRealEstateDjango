d7229a53e41777fde8138172cba661db044e048f
umangd98 <umangd98@gmail.com>
2020-03-28 16:49:40 +0530

    final

---
+++ accounts/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages,auth
+from django.contrib.auth.models import User
+from contacts.models import Contact
+# Create your views here.
+def register(request):
+  if request.method == 'POST':
+    #register user
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    username = request.POST['username']
+    email = request.POST['email']
+    password = request.POST['password']
+    password2 = request.POST['password2']
+    # check if passwords match
+    if password == password2:
+      if User.objects.filter(username=username).exists():
+        messages.error(request, 'That username is already taken')
+        return redirect('register')
+      else:
+        if User.objects.filter(email=email).exists():
+          messages.error(request, 'That email is already taken')
+          return redirect('register')
+        else:
+          user = User.objects.create_user(username=username, email=email, password=password, first_name=first_name, last_name=last_name)
+          auth.login(request, user)
+          messages.success(request, 'User Created, Logged in')
+          return redirect('index')
+    else:
+      messages.error(request, 'Passwords do not match')
+      return redirect('register')
+      
+  else:
+    return render(request, 'accounts/register.html')
+
+
+def login(request):
+  if request.method == 'POST':
+    #login user
+    username = request.POST['username']
+    password = request.POST['password']
+    user = auth.authenticate(username=username, password=password)
+    if user is not None:
+      auth.login(request, user)
+      messages.success(request, 'Logged In')
+      return redirect('dashboard')
+    else:
+      messages.error(request, 'Invalid Credentials')
+      return redirect('login')
+  else:
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+  if request.method == 'POST':
+    auth.logout(request)
+    messages.success(request,'Logged Out')
+    return redirect('index')
+
+def dashboard(request):
+  user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+  context = {
+    'contacts':user_contacts
+  }
+  return render(request, 'accounts/dashboard.html', context)
--- btre/settings.py
+++ btre/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pages',
+    'listings',
+    'realtors',
+    'django.contrib.humanize',
+    'accounts',
+    'contacts'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +60,7 @@ ROOT_URLCONF = 'btre.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +81,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': 'Umang@123',
+        'HOST': 'localhost'
     }
 }
 
@@ -116,5 +125,26 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'btre/static'),
+)
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+# messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+
+}
+
+#email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'umangd98@gmail.com'
+EMAIL_HOST_PASSWORD = '******'
+EMAIL_USE_TLS = True
